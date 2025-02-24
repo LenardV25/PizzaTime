97b29be3c2aebe0295a31aea44b59be688f200ac
LenardV25 <25villal@retsdstudent.ca>
2025-02-24 17:29:12 +0000

    validated inputs

--- src/main/java/PizzaTime.java
+++ src/main/java/PizzaTime.java
@@ -35,14 +35,14 @@ public class PizzaTime {
 
 
         String must = "Winnipeg";
-        boolean valid = false;
+        boolean validCity = false;
 
-         while (!valid) {
+         while (!validCity ) {
             System.out.println("City Name?");
             String city = scanner.nextLine();
 
             if (city.equals(must)) {
-                valid = true;
+                validCity = true;
             }
             else {
                 System.out.println("Please enter another valid city");
@@ -50,10 +50,7 @@ public class PizzaTime {
         }
         
         //A1A 1A1
-        //computers count from; 0,1,2
-        System.out.println("Postal Code? Captial Letters!");
-        String postal = scanner.nextLine();
-
+        //computers count from 0,1,2
         ArrayList<Integer> postalNum = new ArrayList<>();   //possible postal nums, no zeros
         postalNum.add(1);
         postalNum.add(2);
@@ -65,7 +62,7 @@ public class PizzaTime {
         postalNum.add(8);
         postalNum.add(9);
 
-        ArrayList<String> firstCharsNot = new ArrayList<>();    //not first characters
+        ArrayList<String> firstCharsNot = new ArrayList<>();    //first characters only not allowed
         firstCharsNot.add("D");
         firstCharsNot.add("F");
         firstCharsNot.add("I");
@@ -75,7 +72,7 @@ public class PizzaTime {
         firstCharsNot.add("W");
         firstCharsNot.add("Z");
 
-        ArrayList<String> firstChars = new ArrayList<>();
+        ArrayList<String> firstChars = new ArrayList<>();   //characters allowed throughout
         firstChars.add("D");
         firstChars.add("F");
         firstChars.add("I");
@@ -84,53 +81,50 @@ public class PizzaTime {
         firstChars.add("U");
 
         //A1A 1A1
-        boolean goodPostal;
+        boolean goodPostal = false;
 
-        while (!goodPostal) {
-            char firstChar = postal.charAt(0);
-            
-            if (firstCharsNot.contains(firstChar)) {
+        while (goodPostal == true) {
+            System.out.println("Postal Code? Captial Letters");
+            String postal = scanner.nextLine();
+
+            char first = postal.charAt(0);
+            if (firstCharsNot.contains(first)) {
                 goodPostal = false;
             }
             else {
                 goodPostal = true;
-    
             }
 
-            char secChar = postal.charAt(1);
-            
-            if (postalNum.contains(secChar)) {
+            char sec = postal.charAt(1);
+            if (postalNum.contains(sec)) {
                 goodPostal = true;
-                
             }
             else goodPostal = false;
 
             char third = postal.charAt(2);
             boolean charct;
-            if (postalNum.contains(third)) {
+            if (firstChars.contains(third)) {
                 charct = true;
             }
             else charct = false;
 
-            char space = postal.charAt(3);
-            boolean charSpace;
-            if (postalNum.contains(" ")) {
-            charSpace = true;
-            }
-            else {
-            System.out.println("Please retry with a space");
-            charSpace = false;
-            }
+            char between = postal.charAt(3);
+            boolean isSpace = Character.isSpaceChar(between);
+            if (isSpace = false) {
+                System.out.println("Please add a space in between");
+                goodPostal = false;
+            } else goodPostal = true;
 
+            
             char fourth = postal.charAt(4);
             boolean isFourth;
             if (postalNum.contains(fourth)){
                 isFourth = true;
             } else isFourth = false;
 
-            char fifthChar = postal.charAt(5);
-            boolean isFifthChar,
-            if (postalNum.contains(fifthChar)) {
+            char fifth = postal.charAt(5);
+            boolean isFifthChar;
+            if (firstChars.contains(fifth)) {
                 isFifthChar = true;
             } else isFifthChar = false;
 
